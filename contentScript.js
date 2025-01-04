3c90f2705b097446740c6292fe2dd0a763290691
Andrew Yuan <ayuan124@umd.edu>
2025-01-04 11:50:31 -0500

    Scrape names under "Player Points"

--- contentScript.js
+++ contentScript.js
@@ -1,59 +1,91 @@
+// Aria Labels
 const SPREAD_BETTING_ARIA = 'div[aria-label*="Spread Betting,"]';
+const TEN_POINTS_ARIA = 'div[aria-label*="To Score 10+ Points,"]';
+
+// Numberic Constants
 const REFRESH_INTERVAL = 2000;
 const MAX_RETRIES = 30;
 const CURRENT_SEASON = 2025;
 
 // API Endpoint
-const ESPN_GET_ALL_PLAYERS_URL = "https://sports.core.api.espn.com/v3/sports/basketball/nba/athletes?limit=20000"
-const ESPN_GET_STATS_FROM_ID_URL = `https://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/seasons/${CURRENT_SEASON}/types/2/athletes/`
+const ESPN_GET_ALL_PLAYERS_URL =
+  "https://sports.core.api.espn.com/v3/sports/basketball/nba/athletes?limit=20000";
+const ESPN_GET_STATS_FROM_ID_URL = `https://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/seasons/${CURRENT_SEASON}/types/2/athletes/`;
 
+// Need to use intervals because FanDuel does not load information instantly
+// Use interval to check if info is loaded periodically
 chrome.runtime.onMessage.addListener(async (obj, sender, response) => {
-    // Needed because FanDuel does not load the odds instantly
-    // Check if the spread element has been populated before proceeding
+  if (obj == "MAIN") {
     var counter = 0;
     const intervalId = setInterval(async () => {
-        const spreadElements = document.querySelectorAll(SPREAD_BETTING_ARIA);
-        if (spreadElements.length > 0) {
-            // Scrape spread from page
-            processOdds(spreadElements[0].children[0]);
-            clearInterval(intervalId);
-
-            // Once scraped player, find ESPN id from name and then stats from ID
-            var id = await GetESPNIdFromPlayerName("LeBron", "James");
-            console.log(id);
-            await getStatsFromESPNPlayerID(id);
-        }
-        counter += 1;
-        if (counter > MAX_RETRIES) {
-            console.log("couldn't find div");
-            clearInterval(intervalId);
-        }
+      const spreadElements = document.querySelectorAll(SPREAD_BETTING_ARIA);
+      if (spreadElements.length > 0) {
+        // Scrape spread from page
+        processOdds(spreadElements[0].children[0]);
+        clearInterval(intervalId);
+      }
+      counter += 1;
+      if (counter > MAX_RETRIES) {
+        console.log("couldn't find div");
+        clearInterval(intervalId);
+      }
     }, REFRESH_INTERVAL);
+  } else if (obj == "PLAYERPOINTS") {
+    var counter = 0;
+    const intervalId = setInterval(async () => {
+      const playerElements = document.querySelectorAll(TEN_POINTS_ARIA);
+      if (playerElements.length > 0) {
+        const playerNames = [...playerElements].map(
+          (div) => div.ariaLabel.split(", ")[1],
+        );
+        clearInterval(intervalId);
+
+        // Process the player names and get stats
+        playerNames.forEach((playerName) => processPlayerPoints(playerName))
+      }
+      counter += 1;
+      if (counter > MAX_RETRIES) {
+        console.log("couldn't find div");
+        clearInterval(intervalId);
+      }
+    }, REFRESH_INTERVAL);
+  }
 });
 
 function processOdds(oddsElement) {
-    // To add
-    console.log(oddsElement.innerHTML);
+  // To add
+  console.log(oddsElement.innerHTML);
+}
+
+function processPlayerPoints(playerName) {
+  console.log(playerName);
+  // Once scraped player, find ESPN id from name and then stats from ID
+  //var id = await GetESPNIdFromPlayerName("LeBron", "James");
+  //console.log(id);
+  //await getStatsFromESPNPlayerID(id);
 }
 
 async function GetESPNIdFromPlayerName(playerFirstName, playerLastName) {
-    const options = {
-        method: "GET",
-    };
-    const resp = await fetch(ESPN_GET_ALL_PLAYERS_URL, options);
-    const json = await resp.json();
-    filtered = json.items.filter(
-        (x) => x.firstName === playerFirstName && x.lastName === playerLastName,
-    );
-    return filtered[0].id;
+  const options = {
+    method: "GET",
+  };
+  const resp = await fetch(ESPN_GET_ALL_PLAYERS_URL, options);
+  const json = await resp.json();
+  filtered = json.items.filter(
+    (x) => x.firstName === playerFirstName && x.lastName === playerLastName,
+  );
+  return filtered[0].id;
 }
 
 async function getStatsFromESPNPlayerID(id) {
-    const options = {
-        method: "GET",
-    };
-    const resp = await fetch(ESPN_GET_STATS_FROM_ID_URL + `${id}/statistics/`, options);
-    const json = await resp.json();
-    console.log(json);
-    return json;
+  const options = {
+    method: "GET",
+  };
+  const resp = await fetch(
+    ESPN_GET_STATS_FROM_ID_URL + `${id}/statistics/`,
+    options,
+  );
+  const json = await resp.json();
+  console.log(json);
+  return json;
 }
